2efa37a6624d385a82e6dbc311c0c412ab0a4057
Iury <iurysena74@gmail.com>
2026-06-07 18:03:09 -0300

    feat: inicializa variáveis e pinos para o módulo do display

--- src/modules/main.asm
+++ src/modules/main.asm
@@ -1,1 +1,17 @@
+.nolist
+.include "m328Pdef.inc"
+.list
 
+.equ DEZENA_PIN = PB5
+.equ UNIDADE_PIN = PB4
+.equ DISPLAY = PORTC
+
+.def UNIDADE = R17
+.def DEZENA = R18
+
+main:
+
+
+.nolist
+.include "display.inc"
+.list
