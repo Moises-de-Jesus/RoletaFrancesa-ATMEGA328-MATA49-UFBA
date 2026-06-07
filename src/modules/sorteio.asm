c2445c39fbc79e5ffc3f0a145e921f6a465dc1d5
Moises de Jesus <moises.de.jesus.profissional@gmail.com>
2026-06-07 18:18:08 -0300

    fix: labels with wrong names

--- src/modules/sorteio.asm
+++ src/modules/sorteio.asm
@@ -1,22 +1,24 @@
 start_sort:
     PUSH R17
     LDI R17, 0xFF
-    RCALL loop_sorteio
+    RCALL loop_sort
     POP R17
 
 loop_sort:
     INC R17
     CMP R17, 37
     BREQ zerar
-    RJMP loop_sorteio
-
-zerar:
-    LDI R17, 0xFF
     RJMP loop_sort
 
 loop_fim:
     MOV ganhador, R17
     RET
+
+zerar:
+    LDI R17, 0xFF
+    RJMP loop_sort
+
+
     
 
 
