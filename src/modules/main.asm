086b47f0fe84edc4728b8739df8703a4350693d0
Pedro Simões <pedrokhpms@gmail.com>
2026-06-13 09:00:19 -0300

    feat: adição dos comentários no main e da documentação acerca da lógica de interrupção

--- src/modules/main.asm
+++ src/modules/main.asm
@@ -2,6 +2,7 @@
 .include "m328Pdef.inc"
 .list
 
+; Definições de hardware
 .equ DEZENA_PIN = PC3
 .equ UNIDADE_PIN = PC4
 .equ DISPLAY = PORTB
@@ -11,6 +12,8 @@
 .equ LED_VERDE = PC1
 .equ LED_VERMELHO = PC2
 
+
+; Registradores utilizados
 .def UNIDADE = R17
 .def DEZENA = R18
 
@@ -23,34 +26,49 @@
 .def TEMP = R25
 .def LOOP = R24
 
+; Vetor de reset
+
 .ORG 0x0000 		 
 JMP main  
+
+; Vetor da interrupção INT0
 
 .ORG 0x0002 		  
 JMP interruptRoutine  
 
 main:
+    ; Valor utilizado na lógica de conversão das dezenas
     LDI SALTO_DEZENAS, 0xA
+
+    ; Inicialização da pilha (stack)
     LDI R16,HIGH(RAMEND)  
     OUT SPH,R16  
     LDI R16,LOW(RAMEND)  
     OUT SPL,R16 		 
 
+    ; Configura INT0 para disparar na borda de descida
+    ; (quando o botão é pressionado)
     LDI R16,0x02 		  
     STS EICRA,R16		
 
+    ; Habilita a interrupção externa INT0
     LDI R16,0x01		
     OUT EIMSK,R16		
     SEI 				  
 
+    ; PD2 = botão (entrada)
+    ; PD3 = segmento G do display (saída)
 	LDI R16,0b000001000
-	OUT DDRD,R16 ; botão como entrada e pin g do display como saída 
-    LDI R16,0b000000100 
-    OUT PORTD,R16 ; pull up do botão
+	OUT DDRD,R16  
 
+    ; Ativa resistor pull-up interno do botão 
+    LDI R16,0b000000100 
+    OUT PORTD,R16 
+
+    ; Configura display e LEDs como saída
     LDI R16,0xFF
-	OUT DDRB,R16 ; configura display como saída
-    OUT DDRC,R16 ; configurando leds e transitores como saída
+	OUT DDRB,R16
+    OUT DDRC,R16 
 
 	; Inicializa os displays
 	LDI UNIDADE,0
@@ -59,14 +77,21 @@ main:
 
 	RJMP start_sort	
 
+; Executada quando o botão é pressionado
 interruptRoutine:	
+	; Executa animação visual do sorteio
 	RCALL start_anim
+
+    ; Copia o número sorteado para os displays
 	MOV UNIDADE,GANHADOR_UNIDADE
 	MOV DEZENA,GANHADOR_DEZENA
+
+    ; Acende o LED correspondente à cor sorteada
 	RCALL LIGAR_LED   
 
     RETI
 
+; Inclusão dos módulos do projeto
 .nolist
 .include "display.asm"
 .include "corLed.asm"
